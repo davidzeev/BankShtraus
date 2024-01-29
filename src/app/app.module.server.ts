ff408ac313df3ca89ad6cc99f03d57334bfca944
David Aria Zeev <davidzee@hac.ac.il>
2024-01-29 23:04:01 +0200

    הוספת קבצים ראשונים לפרויקט

--- src/app/app.module.server.ts
+++ src/app/app.module.server.ts
@@ -3,12 +3,21 @@ import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './modules/shared/shared.module';
+import { MainModule } from './modules/main/main.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
+  declarations: [
+
+  ],
   imports: [
     AppModule,
     ServerModule,
+    SharedModule,
+    MainModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent],
 })
-export class AppServerModule {}
+export class AppServerModule { }
